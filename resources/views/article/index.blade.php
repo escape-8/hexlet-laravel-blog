1d946541dce2fd6ec9389cfa1210925533120d73
Mikhail Tsepelev <cplvmhl@gmail.com>
2024-01-03 12:44:39 +0300

    feat: add create articles

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
+@if (session()->has('status'))
+    <div>{{ session('status') }}</div>
+@endif
+
 @section ('content')
+    <a href="{{ route('articles.create') }}">Добавить статью</a>
     <h1>Список статей</h1>
     @foreach($articles as $article)
         <a href="{{ route('articles.index') }}/{{ $article->id }}"><h2>{{$article->name}}</h2></a>
